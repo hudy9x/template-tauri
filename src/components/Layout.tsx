0057e6edb02ee240e3b349fff25fd04cd0289fad
hudy@mac-air <hudy9x@gmail.com>
2026-01-05 15:32:44 +0700

    clean /home page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: LayoutProps) {
   return (
     <>
       {/* MainContent - Between Titlebar and Footer */}
-      <main className="fixed top-[35px] bottom-8 h-[calc(100vh-35px-32px)] left-0 right-0 overflow-auto bg-secondary">
+      <main className="fixed top-[35px] bottom-8 h-[calc(100vh-35px-32px)] left-0 right-0 overflow-auto">
         {children}
       </main>
 
